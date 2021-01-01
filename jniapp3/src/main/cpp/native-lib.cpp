4d1de5939fadff176b579ea4f6f7890c52107646
Saurabh-12 <saurabh.sharma569@gmail.com>
2021-01-01 22:39:30 +0530

    Handling Java exception in Native

--- jniapp3/src/main/cpp/native-lib.cpp
+++ jniapp3/src/main/cpp/native-lib.cpp
@@ -1,7 +1,8 @@
 #include <jni.h>
 #include <string>
 #include <android/log.h>
-#define APPNAME "JniApp2"
+
+#define APPNAME "JniApp3"
 
 extern "C" JNIEXPORT jstring JNICALL
 Java_com_saurabh_jni_jniapp3_MainActivity_stringFromJNI(
@@ -10,4 +11,32 @@ Java_com_saurabh_jni_jniapp3_MainActivity_stringFromJNI(
     std::string hello = "Hello from C++";
     __android_log_print(ANDROID_LOG_VERBOSE, APPNAME, "Saurabh Sharma");
     return env->NewStringUTF(hello.c_str());
+}
+
+extern "C" JNIEXPORT void JNICALL
+Java_com_saurabh_jni_jniapp3_MainActivity_handleExceptionFromNative(JNIEnv *env, jobject obj)
+{
+    jclass cls = env->GetObjectClass(obj);
+    jmethodID mid = env->GetMethodID(cls, "NPETest", "()V");
+    jthrowable exc;
+    if (mid == 0) {
+        return;
+    }
+    env->CallVoidMethod(obj, mid);
+    exc = env->ExceptionOccurred();
+    if (exc) {
+        /* We don't do much with the exception, except that we print a
+           debug message using ExceptionDescribe, clear it, and throw
+           a new exception. */
+        jclass newExcCls;
+
+        env->ExceptionDescribe();
+        env->ExceptionClear();
+
+        newExcCls = env->FindClass( "java/lang/IllegalArgumentException");
+        if (newExcCls == 0) { /* Unable to find the new exception class, give up. */
+            return;
+        }
+        env->ThrowNew(newExcCls, "thrown from C code");
+    }
 }
